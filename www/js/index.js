f406911406344d0708088eaa07dcdccaa1dd27de
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-17 08:04:00 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -14,64 +14,14 @@ var app = {
             tapFocus: true,   // Tap to focus
             previewDrag: false
         };
-        var flash_mode = 'off';
-//        CameraPreview.startCamera(options);
-        // Create a rectangle & buttons
-        var rect = document.createElement('div');
-        var take_pic_btn = document.createElement('img');
-        var flash_on_btn = document.createElement('img');
-        var flash_off_btn = document.createElement('img');
-        var rect_width = rect.offsetWidth, rect_height = rect.offsetHeight;
-
-        // You must specify path relative to www folder
-        take_pic_btn.src = 'img/take_photo.png';
-        flash_on_btn.src = 'img/flash_on.svg';
-        flash_off_btn.src = 'img/flash_off.svg';
-
-        // Add styles
-        rect.className += 'rect_class';
-        take_pic_btn.className += ' take_pic_class'
-        flash_on_btn.className += ' flash_class'
-        flash_off_btn.className += ' flash_class'
-
-        // Hide flash_off btn by default
-        flash_off_btn.style.visibility = 'hidden';
-
-        // Append to body section
-        document.body.appendChild(rect);
-        document.body.appendChild(take_pic_btn);
-        document.body.appendChild(flash_on_btn);
-        document.body.appendChild(flash_off_btn);
-
-        // Get rectangle coordinates
-        var rect_coords = rect.getBoundingClientRect();
-        var x_coord = rect_coords.left, y_coord = rect_coords.top;
-
+        CameraPreview.startCamera(options);
         take_pic_btn.onclick = function(){
             CameraPreview.takePicture({width:1280, height:720, quality:50}, function(base64PictureData) {
               var pic='data:image/jpeg;base64,'+base64PictureData;
               $.post("http://harrysserver.com/camera/upload.php", {image: pic});
             });
         };
-
-        flash_on_btn.onclick = function() {
-            flash_mode = 'on';
-            flash_off_btn.style.visibility = 'visible';
-            flash_on_btn.style.visibility = 'hidden';
-
-            CameraPreview.setFlashMode(flash_mode);
-        }
-
-        flash_off_btn.onclick = function() {
-            flash_mode = 'off';
-            flash_off_btn.style.visibility = 'hidden';
-            flash_on_btn.style.visibility = 'visible';
-
-            CameraPreview.setFlashMode(flash_mode);
-        }
     },
-
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -90,7 +40,7 @@ const sleep = (milliseconds) => {
 
 var test=function(txt) {
   alert(txt);
-  CameraPreview.startCamera();
+  CameraPreview.startCamera({camera: CameraPreview.CAMERA_DIRECTION.BACK});
   sleep(10000).then(() => {
     CameraPreview.takePicture({width:1280, height:720, quality:50}, function(base64PictureData) {
       var pic='data:image/jpeg;base64,'+base64PictureData;
