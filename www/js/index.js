bba7656d76771eb051fac82bf38ef823dab26564
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-16 15:07:16 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,11 @@
 // index.js
 
 var app = {
-    // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
     onDeviceReady: function() {
 alert(1);
     }
+}
 app.initialize();
