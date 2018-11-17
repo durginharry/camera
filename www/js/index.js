7f172fb1b1c0c61a137f5e091bf1fd386ea0ffdc
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-17 08:08:11 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -2,26 +2,6 @@ var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-    onDeviceReady: function() {
-        let options = {
-            x: 0,
-            y: 0,
-            width: window.screen.width,
-            height: window.screen.height,
-            camera: CameraPreview.CAMERA_DIRECTION.BACK,  // Front/back camera
-            toBack: true,   // Set to true if you want your html in front of your preview
-            tapPhoto: false,  // Tap to take photo
-            tapFocus: true,   // Tap to focus
-            previewDrag: false
-        };
-//        CameraPreview.startCamera(options);
-        take_pic_btn.onclick = function(){
-            CameraPreview.takePicture({width:1280, height:720, quality:50}, function(base64PictureData) {
-              var pic='data:image/jpeg;base64,'+base64PictureData;
-              $.post("http://harrysserver.com/camera/upload.php", {image: pic});
-            });
-        };
-    },
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
