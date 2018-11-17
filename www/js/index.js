e5b203d54514c6fd58cf68bdca5ae67aec810131
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-16 15:57:14 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -4,18 +4,19 @@ function takePhoto() {
 alert(1);
   CameraPreview.takePicture(function(cameraData) {
 alert(2);
-    photo='data:image/jpeg;base64,'+cameraData;
+//    photo='data:image/jpeg;base64,'+cameraData;
 alert(3);
-    $.post("http://harrysserver.com/camera/upload.php",
-                        {
-                            // Data sent along with a request
-                            image: cropped_img_base64
-                        },
-                        function(data, status, xhr) {
-                            // Success callback
-                            alert('Status: ' + status + '\nData: ' + data);
-                        }
-                    );
+//    $.post("http://harrysserver.com/camera/upload.php",
+//                        {
+//                            // Data sent along with a request
+//                            image: cropped_img_base64
+//                        },
+//                        function(data, status, xhr) {
+//                            // Success callback
+//                            alert('Status: ' + status + '\nData: ' + data);
+//                        }
+//                    );
+alert(4);
   });
 }
 var app = {
