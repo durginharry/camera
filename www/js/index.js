e1f24b113350ef7a669c0a2b82320f82b77e4577
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-16 14:43:02 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,15 @@
 // index.js
 
-var app = {
-    // Application Constructor
-    initialize: function() {document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);},
-    onDeviceReady: function() {
-        let options = {
-            x: 0,
-            y: 0,
-            width: window.screen.width,
-            height: window.screen.height,
-            camera: CameraPreview.CAMERA_DIRECTION.BACK,  // Front/back camera
-            toBack: true,   // Set to true if you want your html in front of your preview
-            tapPhoto: false,  // Tap to take photo
-            tapFocus: false,   // Tap to focus
-            previewDrag: false
-        };
-        CameraPreview.startCamera(options);
-alert(1);
-        CameraPreview.takePicture(function(base64PictureData) {
-alert(2);
-          var crop_func = crop(base64PictureData, 640, 640, 0, 0, function(cropped_photo) {
-alert(3);
-            $.post("http://harrysserver.com/camera/upload.php", {image: base64PictureData}, function(data, status, xhr) {alert('success');})
-            .fail(function(error, status, xhr) {alert('fail');});
-alert(4);
-          });
-        });
-    }
-}
+var app = {initialize: function() {document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);};};
+
+function takephoto() {
+  alert(2);
+  CameraPreview.takePicture({width: app.dimension.width, height: app.dimension.height}, function(photo){
+    $.post("http://harrysserver.com/camera/upload.php", {image: 'data:image/jpeg;base64,'+photo});
+  });
+};
+  
 
 app.initialize();
+  alert(1);
+takephoto();
