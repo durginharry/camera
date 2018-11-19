b2d2c4448ad1a60836690e0b6c47d86d7f56d73e
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-19 08:43:46 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -30,11 +30,12 @@ async function processArray() {
 }
 
 app.initialize();
-alert('1');
+CameraPreview.getFocusMode(function (f){ alert('1');});
 //CameraPreview.getFocusMode(function(currentFocusMode){ alert(currentFocusMode); });
 //CameraPreview.setFocusMode('fixed');
 //CameraPreview.setFlashMode('off');
 //CameraPreview.startCamera({camera: CameraPreview.CAMERA_DIRECTION.BACK});
+alert('2');
 //CameraPreview.hide();
 //processArray();
 
