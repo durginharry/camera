e4cf20353f188ce936f6359a917724de52ff8544
durginharry <44795620+durginharry@users.noreply.github.com>
2018-11-20 08:11:05 -1000

    ver

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,7 @@ var app = {
 };
 
 var photograph=function() {
-  let url='http://harrysserver.com/camera/upload.php';
+  let url='http://harrysserver.com/camera/windward.php';
       CameraPreview.setFlashMode('off');
       CameraPreview.takePicture({height:1920, width:1080, quality:90}, function(base64PictureData) {
         var pic='data:image/jpeg;base64,'+base64PictureData;
